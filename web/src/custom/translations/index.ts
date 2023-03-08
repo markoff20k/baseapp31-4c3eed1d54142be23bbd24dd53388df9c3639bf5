6ea933920f3fead3d767ee6b2d332ecc7db78256
Jeferson Martin de Araujo <jmartin20k@gmail.com>
2023-03-08 15:39:51 -0300

    v2

--- web/src/custom/translations/index.ts
+++ web/src/custom/translations/index.ts
@@ -1,6 +1,5 @@
-import { en } from './en';
 import { pt } from './pt';
 
 export const customLanguageMap = {
-    en, pt
+    pt,
 };
--- web/src/translations/index.ts
+++ web/src/translations/index.ts
@@ -1,11 +1,14 @@
 import { customLanguageMap } from '../custom/translations';
 import { en as enCustom } from '../custom/translations/en';
+import { pt as ptCustom } from '../custom/translations/pt';
 import { en } from './en';
+import { pt } from './pt';
 
 export type LangType = typeof en;
 
 export const languageMap = {
     default: en,
     en: { ...en, ...enCustom },
+    pt: { ...pt, ...ptCustom },
     ...customLanguageMap,
 };
